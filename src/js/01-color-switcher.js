ade4b407b25b8536b3315d60c082c751e79b1826
Nataliia-Shevchenko <nata_yuna@ukr.net>
2023-03-14 13:26:47 +0200

    add task 1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,26 @@
+const bodyEl = document.querySelector('body');
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
+
+startBtnEl.addEventListener('click', handleStartBtnClick);
+
+stopBtnEl.addEventListener('click', handleStopBtnClick);
+
+function handleStartBtnClick(e) {
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  startBtnEl.disabled = true;
+  stopBtnEl.disabled = false;
+}
+
+function handleStopBtnClick(e) {
+  clearInterval(timerId);
+  startBtnEl.disabled = false;
+  stopBtnEl.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
